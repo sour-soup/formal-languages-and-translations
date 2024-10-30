d1d4ddef43a90188ffc1834f38645825d6aaa53c
sour-soup <kiziyepi8@gmail.com>
2024-10-31 01:29:59 +0400

    fix(laba1): removed unnecessary imports

--- src/main/java/laba1/Main.java
+++ src/main/java/laba1/Main.java
@@ -1,12 +1,9 @@
 package laba1;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.List;
-import java.util.stream.Collectors;
 
 public class Main {
     public static void main(String[] args) {
