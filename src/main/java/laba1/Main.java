bb13cb9081f3698c92a4cb4f04ee83995e29a876
sour-soup <kiziyepi8@gmail.com>
2024-10-31 01:28:55 +0400

    feat(laba1): change file input

--- src/main/java/laba1/Main.java
+++ src/main/java/laba1/Main.java
@@ -3,23 +3,20 @@ package laba1;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Main {
     public static void main(String[] args) {
-        StringBuilder inputBuilder = new StringBuilder();
-
-        try (BufferedReader reader = new BufferedReader(new FileReader("input.txt"))) {
-            String line;
-            while ((line = reader.readLine()) != null) {
-                inputBuilder.append(line).append("\n");
-            }
+        String input;
+        try {
+            input = String.join("", Files.readAllLines(Paths.get("input.txt")));
         } catch (IOException e) {
-            System.err.println(e.getMessage());
-            return;
+            throw new RuntimeException(e);
         }
 
-        String input = inputBuilder.toString();
         List<Lexeme> lexemes = LexemeAnalyzer.analyze(input);
         LexemeTablePrinter.printLexemeTable(lexemes);
     }
